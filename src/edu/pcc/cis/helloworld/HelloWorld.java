5e0e7fd7977a3404e215d0a8fab419301ac3f867
Max Robinson <Maxwellrobinson99@gmail.com>
2021-01-30 14:51:54 -0800

    print date

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -5,10 +5,12 @@ package edu.pcc.cis.helloworld;
  * CIS 233J Lab 4
  * 1/30/2021
  */
+import  java.util.Date;
 
 public class HelloWorld {
     public static void main(String[] args)
     {
         System.out.println("Hello, World!");
+        System.out.println(new Date());
     }
 }
