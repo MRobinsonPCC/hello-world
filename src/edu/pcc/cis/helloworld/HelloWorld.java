0f8ff47e0a5549429e8a73f2ad56f817ac05dd48
Max Robinson <Maxwellrobinson99@gmail.com>
2021-01-30 14:50:04 -0800

    added main method that prints hello world

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,4 +1,14 @@
 package edu.pcc.cis.helloworld;
 
+/**
+ * Maxwell Robinson
+ * CIS 233J Lab 4
+ * 1/30/2021
+ */
+
 public class HelloWorld {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, World!");
+    }
 }
